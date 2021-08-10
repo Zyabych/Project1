651139881ace0e667bd2b0bceec27a2d218b751d
MINSK\Pavel_Medlev <pavel_medlev@epam.com>
2021-08-10 15:45:48 +0300

    review

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Threading;
 
-namespace MaxOfArray
+namespace Project1
 {
     class Program
     {
@@ -9,46 +8,36 @@ namespace MaxOfArray
         {
             int[,] arrayTable = new int[3,35];
             int[] table = new int[106];  
-            Random random = new Random();
-            int k = 0;
-            for (int i = 0; i<3; i++)
+
+            var random = new Random();
+            var k = 0;
+
+            for (var i = 0; i < 3; i++)
             {
-                for (int j = 0; j < 35; j++)
+                for (var j = 0; j < 35; j++)
                 {
-
-                    arrayTable[i, j] = random.Next(10,100);
-                    Console.Write($"{arrayTable[i, j]} ");
+                    arrayTable[i, j] = random.Next(10, 100);
+                    Console.Write(arrayTable[i, j]);
                     table[k++] = arrayTable[i, j];
-
                 }
 
                 Console.WriteLine();
             }
 
-            for (int i = 0; i<table.Length; i++)
+            for (var i = 0; i < table.Length; i++)
             {
-                for (int j = i+1; j<table.Length; j++)
+                for (var j = i + 1; j < table.Length; j++)
                 {
                     if (table[j] > table[i])
                     {
-                        int a = table[i];
+                        var a = table[i];
                         table[i] = table[j];
                         table[j] = a;
-
                     }
                 }
-
             }
 
-            Console.WriteLine("Result: ");
-
-            for (int i = 0; i < 10; i++)
-            {
-                Console.Write($"{table[i]} ");
-
-            }
-
-
+            Console.WriteLine($"Result: {string.Join(", ", table)}");
         }
     }
 }
